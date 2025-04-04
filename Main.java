56ad91d5c4b198e793209c7df450da76ef5e2ee5
Philipp Schubert <philipp.schubert@sonarsource.com>
2025-04-04 11:01:15 +0200

    Add Java and C++ sources

---
+++ Main.java
@@ -0,0 +1,12 @@
+import java.lang.System;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int a = 11;
+        int b = 31;
+        int ans = Compute.add(a, b);
+        System.out.println("Hello, Java!");
+        System.out.println("The answer is: " + ans);
+    }
+}
